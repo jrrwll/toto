02f72dd4ebe387db732bc3274226609aa1f5009f
ituke <tukeof@outlook.com>
2017-06-15 17:07:12 +0800

    Arith0.1.0615

---
+++ Arith/ElementaryInteger.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Toto.Arith
+{
+    public static partial class ElementaryInteger
+    {
+        // 阶乘
+        public static int Factorial(int num)
+        {
+            if (num < 2) return 1;
+            else return num * Factorial(num - 1);
+        }
+        public static long Factorial(long num)
+        {
+            if (num < 2) return 1;
+            else return num * Factorial(num - 1);
+        }
+
+
+        // 最大公约数
+        public static int GCD(int num1, int num2)
+        {
+            int a = Math.Max(num1, num2);
+            int b = Math.Min(num1, num2);
+
+            int m;
+            while (b != 0)
+            {
+                a = a > b ? a : b;
+                m = a % b;
+                a = b;
+                b = m;
+            }
+            return a;
+        }
+        public static long GCD(long num1, long num2)
+        {
+            long a = Math.Max(num1, num2);
+            long b = Math.Min(num1, num2);
+
+            long m;
+            while (b != 0)
+            {
+                a = a > b ? a : b;
+                m = a % b;
+                a = b;
+                b = m;
+            }
+            return a;
+        }
+
+        // 最小公倍数
+        public static int LCM(int num1, int num2)
+        {
+            return (num1 * num2) / GCD(num1, num2);
+        }
+        public static long LCM(long num1, long num2)
+        {
+            return (num1 * num2) / GCD(num1, num2);
+        }
+
+
+        // 素数判定
+        public static bool IsPrime(int num)
+        {
+            if (num <= 1) throw new ArgumentOutOfRangeException("Assert int num >= 2");
+
+            if (num == 2) return true;
+
+
+            for (int i = 3, y = (int)Math.Sqrt(num) + 1; i < y; i = i + 2)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+        public static bool IsPrime(long num)
+        {
+            if (num <= (long)Math.Pow(2, 20)) return IsPrime((int)num);
+
+            bool result = true;
+            long item2 = (long)Math.Sqrt(num) + 1;
+
+            Parallel.ForEach(Partitioner.Create(3, item2), (rang) =>
+            {
+                for (long i = rang.Item1; i < rang.Item2; i = i + 2)
+                {
+                    if (num % i == 0) result = false;
+                }
+            });
+            return result;
+        }
+
+
+    }
+}
---
+++ Arith/PeculiarInteger.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Toto.Arith
+{
+    public static partial class PeculiarInteger
+    {
+
+        // 水仙花数 abc = a^3 + b^3 + c^3
+        public static List<int> GetDaffodil3()
+        {
+            var list = new List<int>();
+            int a, b, c;
+            for (int i = 100; i <= 999; i++)
+            {
+                a = i / 100;
+                Math.DivRem(i, 100, out b);
+                b /= 10;
+                Math.DivRem(i, 100, out c);
+                a = a * a * a;
+                b = b * b * b;
+                c = c * c * c;
+                if ((a + b + c) == i) list.Add(i);
+            }
+            return list.Count == 0 ? null : list;
+        }
+
+        public static List<int> GetDaffodil2()
+        {
+            var list = new List<int>();
+            int a, b;
+            for (int i = 10; i <= 99; i++)
+            {
+                a = i / 10;
+                Math.DivRem(i, 10, out b);
+                a = a * a;
+                b = b * b;
+                if ((a + b) == i) list.Add(i);
+            }
+            return list.Count == 0 ? null : list;
+        }
+
+        public static List<int> GetDaffodil4()
+        {
+            var list = new List<int>();
+            int a, b, c, d;
+            int count = 90000 / Environment.ProcessorCount + 1;
+
+            Parallel.ForEach(Partitioner.Create(1000, 9999, count), (rang) =>
+            {
+                for (int i = rang.Item1; i < rang.Item2; i++)
+                {
+                    a = i / 1000;
+                    Math.DivRem(i, 1000, out b);
+                    b /= 100;
+                    Math.DivRem(i, 100, out c);
+                    c /= 10;
+                    Math.DivRem(i, 10, out d);
+
+                    a = a * a * a * a;
+                    b = b * b * b * b;
+                    c = c * c * c * c;
+                    d = d * d * d * d;
+
+                    if ((a + b + c + d) == i)
+                        list.Add(i);
+                }
+            });
+            return list.Count == 0 ? null : list;
+        }
+
+        public static List<int> GetDaffodil5()
+        {
+            var list = new List<int>();
+            int a, b, c, d, e;
+
+            Parallel.ForEach(Partitioner.Create(10000, 99999), (rang) =>
+            {
+                for (int i = rang.Item1; i < rang.Item2; i++)
+                {
+                    a = i / 10000;
+                    Math.DivRem(i, 10000, out b);
+                    b /= 1000;
+                    Math.DivRem(i, 1000, out c);
+                    c /= 100;
+                    Math.DivRem(i, 100, out d);
+                    d /= 10;
+                    Math.DivRem(i, 10, out e);
+
+                    a = a * a * a * a * a;
+                    b = b * b * b * b * b;
+                    c = c * c * c * c * c;
+                    d = d * d * d * d * d;
+                    e = e * e * e * e * e;
+
+                    if ((a + b + c + d + e) == i)
+                        list.Add(i);
+                }
+            });
+            return list.Count == 0 ? null : list;
+        }
+
+        public static List<int> GetDaffodil(int n)
+        {
+            var list = new List<int>();
+            int[] p = new int[n];
+
+            var item1 = (int)Math.Pow(10, n - 1);
+            var item2 = (int)Math.Pow(10, n) - 1;
+            int sum = 0;
+
+            Parallel.ForEach(Partitioner.Create(item1, item2), (rang) =>
+            {
+                for (int i = rang.Item1; i < rang.Item2; i++)
+                {
+                    for (int j = n - 1; j >= 0; i--)
+                    {
+                        Math.DivRem(i, (int)Math.Pow(10, j + 1), out p[j]);
+                        p[j] /= (int)Math.Pow(10, j);
+                        sum += (int)Math.Pow(p[j], n);
+                    }
+                    if (sum == i)
+                        list.Add(i);
+                }
+            });
+            return list.Count == 0 ? null : list;
+        }
+
+
+    }
+}
